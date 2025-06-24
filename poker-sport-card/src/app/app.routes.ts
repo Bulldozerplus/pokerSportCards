3681c49b1fad34e8f1612618f29a7758e1061283
Nikita <NikitaT-2009@yandex.ru>
2025-06-24 19:08:03 +0300

    add authorization and registration logic on page "auth-page"

--- poker-sport-card/src/app/app.routes.ts
+++ poker-sport-card/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import {AuthComponent} from "../pages/auth-page/auth-page.component";
+import {RulesPageComponent} from "../pages/rules-page/rules-page.component";
+import {GameFieldPageComponent} from "../pages/game-field-page/game-field-page.component";
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: AuthComponent,
+    },
+    {
+        path: 'rules',
+        component: RulesPageComponent,
+    },
+    {
+        path: 'game',
+        component: GameFieldPageComponent,
+    }
+];
